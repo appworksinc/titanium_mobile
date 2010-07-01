62b30dd48d7f908492b040009d3f90b023063568
dasher <dasher@inspiredthinking.co.uk>
2010-07-01 19:28:56 +0200

    Started Contacts for Android

---
+++ android/modules/contacts/src/ti/modules/titanium/contacts/ContactsModule.java
@@ -0,0 +1,17 @@
+package ti.modules.titanium.contacts;
+
+import org.appcelerator.titanium.TiContext;
+import org.appcelerator.titanium.TiModule;
+
+public class ContactsModule extends TiModule {
+
+	public ContactsModule(TiContext tiContext) {
+		super(tiContext);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void showContacts() {
+		
+	}
+
+}
