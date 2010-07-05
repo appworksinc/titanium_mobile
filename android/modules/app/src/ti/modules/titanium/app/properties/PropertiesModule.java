8f7cb0a3bf8283bc461048920bb4196b28a508a9
dasher <dasher@inspiredthinking.co.uk>
2010-07-05 17:19:22 +0200

    Updated Properties to handle defaults better as per Don's suggestion - needed for some tests being run when implementing contacts on Android

--- android/modules/app/src/ti/modules/titanium/app/properties/PropertiesModule.java
+++ android/modules/app/src/ti/modules/titanium/app/properties/PropertiesModule.java
@@ -24,50 +24,178 @@ public class PropertiesModule extends TiModule {
 		super(tiContext);
 		
 		appProperties = tiContext.getTiApp().getAppProperties();
-	}
-
-	public boolean getBool(String key, boolean def) {
-		return appProperties.getBool(key, def);
-	}
-
-	public double getDouble(String key, double def) {
-		return appProperties.getDouble(key, def);
-	}
-
-	public int getInt(String key, int def) {
-		return appProperties.getInt(key, def);
-	}
-
-	public Object[] getList(String key, Object[] def) {
-		String[] defList = new String[0];
-		if (def != null) {
-			defList = new String[def.length];
-			for (int i = 0; i < def.length; i++) {
-				defList[i] = def.toString();
-			}
-		}
 		
-		//auto transform JSON data into objects
-		String values[] = appProperties.getList(key, defList);
-		Object list[] = new Object[values.length];
-		for (int i = 0; i < values.length; i++) {
-			String value = values[i];
-			if (value.startsWith("{") && value.endsWith("}")) {
-				try {
-					list[i] = new TiDict(new JSONObject(value));
-				} catch (JSONException e) {
-					Log.e(LCAT, "Error converting JSON string to TiDict, property:" + key, e);
-				}
-			} else {
-				list[i] = value;
-			}
-		}
-		return list;
 	}
 
-	public String getString(String key, String def) {
-		return appProperties.getString(key, def);
+	public Object getBool(Object[] args) {
+		
+		if (null != args && args.length>0) {
+			// We have some args to work with
+			String key = TiConvert.toString(args[0]);
+			
+			boolean defaultValue = false;
+			if (!appProperties.hasProperty(key)) {
+				// Without being forced to pass in a default - we need to handle the case where there isn't one passed
+				// This means we either have to return null or throw something
+				return null;
+			} else if (args.length > 1) {
+				// grab the default that's been passed
+				defaultValue = TiConvert.toBoolean(args[1]);
+			}			
+			return appProperties.getBool(key, defaultValue);
+		} else {
+			// Should throw something - as we always expect at least a key
+			return null;
+		}
 	}
+	
+	public Object getDouble(Object[] args) {
+		
+		if (null != args && args.length>0) {
+			// We have some args to work with
+			String key = TiConvert.toString(args[0]);
+			
+			double defaultValue = 0.00f;
+			if (!appProperties.hasProperty(key)) {
+				// Without being forced to pass in a default - we need to handle the case where there isn't one passed
+				// This means we either have to return null or throw something
+				return null;
+			} else if (args.length > 1) {
+				// grab the default that's been passed
+				defaultValue = TiConvert.toDouble(args[1]);
+			}			
+			return appProperties.getDouble(key, defaultValue);
+		} else {
+			// Should throw something - as we always expect at least a key
+			return null;
+		}
+	}
+	
+	public Object getInt(Object[] args) {
+		
+		if (null != args && args.length>0) {
+			// We have some args to work with
+			String key = TiConvert.toString(args[0]);
+			
+			int defaultValue = 0;
+			if (!appProperties.hasProperty(key)) {
+				// Without being forced to pass in a default - we need to handle the case where there isn't one passed
+				// This means we either have to return null or throw something
+				return null;
+			} else if (args.length > 1) {
+				// grab the default that's been passed
+				defaultValue = TiConvert.toInt(args[1]);
+			}			
+			return appProperties.getInt(key, defaultValue);
+		} else {
+			// Should throw something - as we always expect at least a key
+			return null;
+		}
+	}	
+	
+	public Object getString(Object[] args) {
+		
+		if (null != args && args.length>0) {
+			// We have some args to work with
+			String key = TiConvert.toString(args[0]);
+			
+			String defaultValue = 0;
+			if (!appProperties.hasProperty(key)) {
+				// Without being forced to pass in a default - we need to handle the case where there isn't one passed
+				// This means we either have to return null or throw something
+				return null;
+			} else if (args.length > 1) {
+				// grab the default that's been passed
+				defaultValue = TiConvert.toString(args[1]);
+			}			
+			return appProperties.getString(key, defaultValue);
+		} else {
+			// Should throw something - as we always expect at least a key
+			return null;
+		}
+	}	
+	
+	public Object[] getList(Object[] args) {
+		
+		if (null != args && args.length>0) {
+			// We have some args to work with
+			String key = TiConvert.toString(args[0]);
+			
+			String[] defaultValue = new String[]{};
+			if (!appProperties.hasProperty(key)) {
+				// Without being forced to pass in a default - we need to handle the case where there isn't one passed
+				// This means we either have to return null or throw something
+				return null;
+			} else if (args.length > 1) {
+				// grab the default that's been passed
+				defaultValue = TiConvert.toStringArray((String[])args[1]);
+			}
+			
+			//auto transform JSON data into objects
+			String values[] = appProperties.getList(key, defaultValue);
+			
+			Object list[] = new Object[values.length];
+			for (int i = 0; i < values.length; i++) {
+				String value = values[i];
+				if (value.startsWith("{") && value.endsWith("}")) {
+					try {
+						list[i] = new TiDict(new JSONObject(value));
+					} catch (JSONException e) {
+						Log.e(LCAT, "Error converting JSON string to TiDict, property:" + key, e);
+					}
+				} else {
+					list[i] = value;
+				}
+			}
+			return list;
+			
+			
+		} else {
+			// Should throw something - as we always expect at least a key
+			return null;
+		}		
+	}
+	
+	
+	// Old methods
+//	public boolean getBool(String key, boolean def) {
+//		return appProperties.getBool(key, def);
+//	}
+//
+//	public double getDouble(String key, double def) {
+//		return appProperties.getDouble(key, def);
+//	}
+//
+//	public int getInt(String key, int def) {
+//		return appProperties.getInt(key, def);
+//	}
+//
+//	public Object[] getList(String key, Object[] def) {
+//		String[] defList = new String[0];
+//		if (def != null) {
+//			defList = new String[def.length];
+//			for (int i = 0; i < def.length; i++) {
+//				defList[i] = def.toString();
+//			}
+//		}
+//		
+//		//auto transform JSON data into objects
+//		String values[] = appProperties.getList(key, defList);
+//		Object list[] = new Object[values.length];
+//		for (int i = 0; i < values.length; i++) {
+//			String value = values[i];
+//			if (value.startsWith("{") && value.endsWith("}")) {
+//				try {
+//					list[i] = new TiDict(new JSONObject(value));
+//				} catch (JSONException e) {
+//					Log.e(LCAT, "Error converting JSON string to TiDict, property:" + key, e);
+//				}
+//			} else {
+//				list[i] = value;
+//			}
+//		}
+//		return list;
+//	}
 
 	public boolean hasProperty(String key) {
 		return appProperties.hasProperty(key);
